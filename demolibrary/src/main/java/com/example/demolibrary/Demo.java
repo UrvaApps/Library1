af24d0cf43dba71614574fb48350fc94f91bcb23
UrvaApps <dexterpune@gmail.com>
2018-11-16 16:06:09 +0530

    Initial Commit

---
+++ demolibrary/src/main/java/com/example/demolibrary/Demo.java
@@ -0,0 +1,11 @@
+package com.example.demolibrary;
+
+import android.content.Context;
+import android.widget.Toast;
+
+public class Demo {
+    public static void s(Context context,String s)
+    {
+        Toast.makeText(context,s,Toast.LENGTH_LONG).show();
+    }
+}
